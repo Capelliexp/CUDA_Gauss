cde42fc33ee3b3bb30bf3c671a15f4f35f5b0114
fipe14 <fipe14@H470-0005>
2017-12-12 14:59:52 +0100

    b4 double time

--- CUDA_Gauss/GPU_Functions.cu
+++ CUDA_Gauss/GPU_Functions.cu
@@ -1,7 +1,7 @@
 #include "project.cuh"
 
 __global__
-void DeviceGaussForward(float* d_m, float* d_v) {
+void DeviceGaussForwardLower(float* d_m, float* d_v) {
 	int id = threadIdx.x + blockIdx.x * blockDim.x + 1;
 
 	if (id < COLUMN_LENGTH) {
@@ -10,7 +10,7 @@ void DeviceGaussForward(float* d_m, float* d_v) {
 
 		__shared__ float pivotRHS;
 		__shared__ float pivotLHS[ROW_LENGTH];
-		
+
 		float RHS = d_v[id];
 		float LHS[ROW_LENGTH];
 
@@ -32,8 +32,63 @@ void DeviceGaussForward(float* d_m, float* d_v) {
 			++i;
 		}
 
-		for (int j = 0; j < ROW_LENGTH; ++j) d_m[ROW_LENGTH*id + j] = LHS[j]; 
+		for (int j = 0; j < ROW_LENGTH; ++j) d_m[ROW_LENGTH*id + j] = LHS[j];
 		d_v[id] = RHS;
+	}
+}
+
+__global__
+void DeviceGaussForwardUpper(float* d_m, float* d_v, float* d_a) {
+	int id = threadIdx.x + blockIdx.x * blockDim.x;
+
+	if (id < COLUMN_LENGTH) {
+		int i = COLUMN_LENGTH-1;
+		float factor;
+
+		__shared__ float pivotRHS;
+		__shared__ float pivotLHS[ROW_LENGTH];
+
+		float RHS = d_v[id];
+		float LHS[ROW_LENGTH];
+
+		for (int j = 0; j < ROW_LENGTH; ++j) {
+			int pos = (id * ROW_LENGTH) + j;
+			float a = d_m[pos];
+			LHS[j] = a;
+			//printf("id %d: LHS[%d] = %f \n",id, j, LHS[j]);
+		}
+
+		while (id < i) {
+			__syncthreads();
+			pivotRHS = d_v[i];
+
+			for (int j = 0; j < ROW_LENGTH; ++j) {
+				pivotLHS[j] = d_m[i*ROW_LENGTH + j];
+			}
+
+			float a = LHS[i];
+			float b = pivotLHS[i];
+			float c = (a / b);
+			factor = c * (-1);
+			//factor = (LHS[i] / pivotLHS[i]) * (-1);
+
+			//if (id == 0) printf("factor: %f \n", factor);
+			for (int j = 0; j < ROW_LENGTH; j++) {
+				LHS[j] += (factor * pivotLHS[j]);
+				//if (id == 0) printf("i %d: LHS[%d] = %f \n",i, j, LHS[j]);
+			}
+			//printf("\n");
+
+			RHS += (factor * pivotRHS);
+
+			--i;
+
+			if (!(id < i)) {
+				for (int j = 0; j < ROW_LENGTH; ++j) d_m[ROW_LENGTH*id + j] = LHS[j];
+				d_v[id] = RHS;
+				d_a[id] = RHS / LHS[i+1];
+			}
+		}
 	}
 }
 
@@ -41,6 +96,11 @@ void InitCUDA(float** m, float* v, float* a) {
 	float* d_m = nullptr;
 	float* d_v = nullptr;
 	float* d_a = nullptr;
+
+	float duration = 0;
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
 
 	float* cuda_m = new float[ROW_LENGTH*COLUMN_LENGTH];
 	for (int i = 0; i < COLUMN_LENGTH; i++){
@@ -56,24 +116,43 @@ void InitCUDA(float** m, float* v, float* a) {
 	cuErrorCheck(cudaMalloc((void**)&d_v, COLUMN_LENGTH * sizeof(float)));
 	cuErrorCheck(cudaMalloc((void**)&d_a, COLUMN_LENGTH * sizeof(float)));
 
+	//Copy memory
 	cuErrorCheck(cudaMemcpy(d_m, cuda_m, ROW_LENGTH * COLUMN_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
 	cuErrorCheck(cudaMemcpy(d_v, v, COLUMN_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
 	cuErrorCheck(cudaMemcpy(d_a, a, COLUMN_LENGTH * sizeof(float), cudaMemcpyHostToDevice));
 
-	DeviceGaussForward<<<2, 2>>>(d_m, d_v);
+	//cudaEventRecord(start);
+	DeviceGaussForwardLower<<<2, 2>>>(d_m, d_v);	//upper
 	cuErrorCheck(cudaGetLastError());
+	//cudaEventRecord(stop);
 
+	//cudaEventSynchronize(stop);
+	//cudaEventElapsedTime(&duration, start, stop);
+	//std::cout << "CUDA 1 time: " << duration / 1000 << " sec" << std::endl;
+
+	//Copy memory
 	cuErrorCheck(cudaMemcpy(cuda_m, d_m, ROW_LENGTH * COLUMN_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
 	cuErrorCheck(cudaMemcpy(v, d_v, COLUMN_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
 
-	/*for (int i = 0; i < ROW_LENGTH * COLUMN_LENGTH; i++) {
-		if (i%ROW_LENGTH == 0) std::cout << std::endl;
-		std::cout << cuda_m[i] << " ";
-	}
-	std::cout << std::endl;*/
+	PrintMatrix(cuda_m, v, a);
+
+	//cudaEventRecord(start);
+	//DeviceGaussForwardUpper<<<2, 2>>>(d_m, d_v, d_a);	//lower
+	cuErrorCheck(cudaGetLastError());
+	//cudaEventRecord(stop);
+
+	//cudaEventSynchronize(stop);
+	//cudaEventElapsedTime(&duration, start, stop);
+	//std::cout << "CUDA 2 time: " << duration / 1000 << " sec" << std::endl;
+
+	//Copy memory
+	cuErrorCheck(cudaMemcpy(cuda_m, d_m, ROW_LENGTH * COLUMN_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
+	cuErrorCheck(cudaMemcpy(v, d_v, COLUMN_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
+	cuErrorCheck(cudaMemcpy(a, d_a, COLUMN_LENGTH * sizeof(float), cudaMemcpyDeviceToHost));
 
 	PrintMatrix(cuda_m, v, a);
 
+	//Free memory
 	cudaFree(d_m);
 	cudaFree(d_v);
 	cudaFree(d_a);
--- CUDA_Gauss/project.cuh
+++ CUDA_Gauss/project.cuh
@@ -9,7 +9,7 @@
 #include "device_launch_parameters.h"
 
 #define COLUMN_LENGTH 4 // y
-#define ROW_LENGTH 4	// x
+#define ROW_LENGTH 4 // x
 
 void FillMAtrixRandom(float** matrix, float* vector);
 void SortCPU(float** matrix, float* vector, float* answer);
@@ -22,4 +22,5 @@ void BackSubstitution(float** matrix, float* vector, float* answer);
 void InitCUDA(float** m, float* v, float* a);
 void cuErrorCheck(cudaError_t cs);
 
-__global__ void DeviceGaussForward(float* d_m, float* d_v);
+__global__ void DeviceGaussForwardLower(float* d_m, float* d_v);
+__global__ void DeviceGaussForwardUpper(float* d_m, float* d_v, float* d_a);
